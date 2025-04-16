1d8277634c9b1941fd1f2f01cc0502e341b4a508
askri bilal <askri.bilal@hotmail.fr>
2025-04-16 11:59:25 +0200

    exo terminé

--- bonjour_JS/hello.js
+++ bonjour_JS/hello.js
@@ -1,6 +1,21 @@
+let prenom = prompt("Saisir un prénom")
+function sayHello(firstname, hour)
+{
 let message = "Bonjour !"
 // console.log(message)
 
-let firstname = "Beyonce !"
-message = "Bonjour " + firstname 
-console.log(message)
+// let firstname = "Beyonce !"
+// message = "Bonjour " + firstname 
+// console.log(message)
+
+let msg = "Bonjour"
+if(hour >= 18)
+{
+    msg = "Bonsoir"
+}
+message = msg + " " + firstname
+document.querySelector('h1').innerText = message;
+}
+sayHello(prenom + "!" ,14)
+// sayHello("Beyoncé !", 22)
+// sayHello("Beyoncé !", 18)
